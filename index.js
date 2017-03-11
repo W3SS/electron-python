544cae52e02147ccc305b262dc60be7f97d79016
unknown <>
2017-03-12 00:27:06 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+function createWindow() {
+    var subpy = require('child_process').spawn('python', ['hello.py'])
+    var rq = require('request-promise')
+    var mainURL = 'http://localhost:5000'
+
+    var openWindow = function() {
+        win = new BrowserWindow({width: 800, height: 600})
+
+        win.loadURL(mainURL)
+
+        win.webContents.openDevTools()
+
+        win.on('closed', () => {
+            win = null
+        })
+    }
+
+    var StartUp = function() {
+        rq(mainURL)
+            .then(function(htmlString) {
+                console.log('Server started.')
+                openWindow()
+            })
+            .catch(function(err) {
+                StartUp()
+            })
+    }
+
+    StartUp()
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
---
+++ readme.md
@@ -0,0 +1,14 @@
+
+# Electron-Python
+
+Requirements:
+
+    installed python 3.6+ (with pip)
+    installed node.js 7.7+ (with npm)
+    pip install Flask
+    npm install -g electron-prebuilt
+
+Usage:
+
+    npm install
+    electron .
